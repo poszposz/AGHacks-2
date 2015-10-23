d03f693d9bd5e0ed92b7eea498c2988b93c33927
Magdalena Pamuła <niceland92@gmail.com>
2015-10-24 00:57:44 +0200

    pink main

--- AGHacks/AGHacks/LoaderViewController.m
+++ AGHacks/AGHacks/LoaderViewController.m
@@ -54,6 +54,8 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         [self.splashView startAnimation];
     });
+    
+    [self performSegueWithIdentifier:@"moveToMain" sender:nil];
 
 }
 
---
+++ AGHacks/AGHacks/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  AGHacks
+//
+//  Created by Magda on 24.10.2015.
+//  Copyright © 2015 VORM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
