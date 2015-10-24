f9aa8489a0ddb7864e7731be9a9b5c8794472338
Magdalena Pamuła <niceland92@gmail.com>
2015-10-24 16:56:49 +0200

    CharacterInfoViewController added

---
+++ AGHacks/AGHacks/CharacterInfoViewController.m
@@ -0,0 +1,37 @@
+//
+//  CharacterInfoViewController.m
+//  AGHacks
+//
+//  Created by Magda on 24.10.2015.
+//  Copyright © 2015 VORM. All rights reserved.
+//
+
+#import "CharacterInfoViewController.h"
+
+@interface CharacterInfoViewController ()
+
+@end
+
+@implementation CharacterInfoViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
